abdb32ef49f10081a3f96efe1ee2c4a8afa6fbf1
Chloe <25387744+qimiko@users.noreply.github.com>
2024-08-29 02:14:57 -0700

    support using stack for args

--- src/syscall-translator.hpp
+++ src/syscall-translator.hpp
@@ -28,9 +28,33 @@ namespace SyscallTranslator {
 		concept is_one_of = (std::same_as<T, Types> || ...);
 	}
 
+	inline std::uint32_t pull_arg(Environment& env, std::uint32_t idx) {
+		if (idx < 4) {
+			// r0 - r3 stores the first 4 arguments
+			return env.current_cpu()->Regs()[idx];
+		} else {
+			// otherwise, our value is on the stack
+			auto sp = env.current_cpu()->Regs()[13];
+			auto sp_offs = (idx - 4) * 4;
+
+			return env.memory_manager()->read_word(sp_offs);
+		}
+	}
+
+	inline void push_arg(Environment& env, std::uint32_t idx, std::uint32_t value) {
+		if (idx < 4) {
+			env.current_cpu()->Regs()[idx] = value;
+		} else {
+			auto sp = env.current_cpu()->Regs()[13];
+			return env.memory_manager()->write_word(sp, value);
+
+			env.current_cpu()->Regs()[13] -= 4;
+		}
+	}
+
 	template <typename T> requires is_one_of<T, std::uint32_t, std::int32_t, float>
 	inline T translate_reg(Environment& env, std::uint32_t& idx) {
-		auto val = env.current_cpu()->Regs()[idx];
+		auto val = pull_arg(env, idx);
 		idx++;
 		return std::bit_cast<T>(val);
 	}
@@ -38,8 +62,8 @@ namespace SyscallTranslator {
 	template <typename T> requires is_one_of<T, std::uint64_t, std::int64_t, double>
 	inline T translate_reg(Environment& env, std::uint32_t& idx) {
 		auto val = std::bit_cast<T>(
-			static_cast<std::uint64_t>(env.current_cpu()->Regs()[idx]) |
-			static_cast<std::uint64_t>(env.current_cpu()->Regs()[idx + 1]) << 32
+			static_cast<std::uint64_t>(pull_arg(env, idx)) |
+			static_cast<std::uint64_t>(pull_arg(env, idx + 1)) << 32
 		);
 		idx += 2;
 
@@ -54,15 +78,15 @@ namespace SyscallTranslator {
 
 	template <typename T> requires is_one_of<T, std::uint32_t, std::int32_t, float>
 	inline void translate_call_arg(Environment& env, std::uint32_t& idx, T value) {
-		env.current_cpu()->Regs()[idx] = std::bit_cast<std::uint32_t>(value);
+		push_arg(env, idx, std::bit_cast<std::uint32_t>(value));
 		idx++;
 	}
 
 	template <typename T> requires is_one_of<T, std::uint64_t, std::int64_t, double>
 	inline void translate_call_arg(Environment& env, std::uint32_t& idx, T value) {
 		auto data = std::bit_cast<std::uint64_t>(value);
-		env.current_cpu()->Regs()[idx] = static_cast<std::uint32_t>(data);
-		env.current_cpu()->Regs()[idx + 1] = static_cast<std::uint32_t>(data >> 32);
+		push_arg(env, idx, static_cast<std::uint32_t>(data));
+		push_arg(env, idx + 1, static_cast<std::uint32_t>(data >> 32));
 		idx += 2;
 	}
 
@@ -111,10 +135,14 @@ namespace SyscallTranslator {
 	 */
 	template <Translatable R = void, Translatable... Args>
 	R call_func(Environment& env, std::uint32_t vaddr, Args... args) {
+		// in this case, the caller has to restore the stack
+		auto sp = env.current_cpu()->Regs()[13];
 		auto arg_idx = 0u;
-		(SyscallTranslator::translate_call_arg(env, arg_idx, args), ...);
 
+		(SyscallTranslator::translate_call_arg(env, arg_idx, args), ...);
 		env.run_func(vaddr);
+
+		env.current_cpu()->Regs()[13] = sp;
 
 		if constexpr (!std::is_void_v<R>) {
 			// this int is wasteful...
